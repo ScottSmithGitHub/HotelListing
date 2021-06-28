45739d6a8e27c0b239c6ea198cf2b17d6f0f396d
ScottSmithGitHub <scott.smith1500@gmail.com>
2021-06-28 13:03:32 -0400

    Added Country and Hotel Get Methods. Fixed UofW DI

--- HotelListing/Startup.cs
+++ HotelListing/Startup.cs
@@ -15,6 +15,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using HotelListing.Configurations;
+using HotelListing.IRepository;
+using HotelListing.Repository;
 
 namespace HotelListing
 {
@@ -43,13 +45,15 @@ namespace HotelListing
             });
 
             services.AddAutoMapper(typeof(MappperInitializer));
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
 
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListing", Version = "v1" });
             });
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(opt => 
+                opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore); ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
